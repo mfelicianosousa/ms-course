1c5e09dde46f2fa56658acf77f09673ed080e696
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-03-12 20:09:09 -0400

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Marcelino', 900.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Flavia Carla', 900.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Ana Paula', 1250.0);
