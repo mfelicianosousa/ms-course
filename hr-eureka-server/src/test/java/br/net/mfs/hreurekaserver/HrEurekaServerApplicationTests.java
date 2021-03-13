dca0d44a570f02908de2fe8b6bfda0ca5caab694
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-03-13 07:58:43 -0400

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/br/net/mfs/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.mfs.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
