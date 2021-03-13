01ba59bb43a90d1395e481cd05543fb8846d942b
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-03-12 22:18:24 -0400

    RestTemplate

--- hr-payroll/src/main/java/br/net/mfs/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/net/mfs/hrpayroll/services/PaymentService.java
@@ -5,15 +5,34 @@
  * */
 package br.net.mfs.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.net.mfs.hrpayroll.entities.Payment;
+import br.net.mfs.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost ;
+	
+	@Autowired
+	private RestTemplate restTemplate ;
+	
 	public Payment getPayment(Long workerId, int days ){
-		return new Payment("Bob", 200.0 , days) ;
+		
+		Map<String, String> uriVariables = new HashMap<>() ;
+		uriVariables.put("id",""+ workerId) ;
+		
+		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables ) ;
+				
+		return new Payment(worker.getName(), worker.getDailyIncome() , days) ;
 		
 	}
 }
